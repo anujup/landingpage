fd52546ac99854572f006be4a42bc57f853cae6e
anujup <au9794117136@gmail.com>
2024-03-05 15:00:01 +0530

    message

--- gitauto.py
+++ gitauto.py
@@ -1,6 +1,26 @@
 import subprocess
-
+import sys
+msg = "message"
 subprocess.run(["git", "add", "."])
-subprocess.run(["git", "commit"])
+subprocess.run(["git", "commit", "-m", msg])
 subprocess.run(["git", "branch", "-M", "main"])
-subprocess.run(["git", "push", "-u", "main", "-anujup", "-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"])
+
+# Define the command to be executed
+command = "git push -u origin main"
+
+# Run the command and capture its output
+process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Pass input to the command
+process.stdin.write("anujup")
+process.stdin.flush()
+
+# Wait for the command to finish and get the output
+stdout, stderr = process.communicate()
+
+# Print the output
+print(stdout.decode())
+
+# Print any errors
+print(stderr.decode())
+
